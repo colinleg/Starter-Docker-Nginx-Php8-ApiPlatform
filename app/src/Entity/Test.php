9810de68dc5ac8bccb9f4dcaf55c7aca0c6be9b0
Colinleg <80259244+colinleg@users.noreply.github.com>
2021-10-26 18:30:04 +0200

    Initial Commit

---
+++ app/src/Entity/Test.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\TestRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TestRepository::class)
+ */
+#[ApiResource]
+class Test
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Nom;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Numero;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getNumero(): ?int
+    {
+        return $this->Numero;
+    }
+
+    public function setNumero(int $Numero): self
+    {
+        $this->Numero = $Numero;
+
+        return $this;
+    }
+}
